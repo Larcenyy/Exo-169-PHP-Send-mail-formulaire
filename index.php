e174f27a8010da30fa42229c37cdac32d2464825
Rémy <remyc59440@hotmail.com>
2023-01-19 10:50:54 +0100

    okk

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<?php
+
+?>
+
+<form action="send.php" method="post">
+    <label for="email">Email:</label>
+    <input type="text" id="email" name="mail">
+
+    <label for="message">Message:</label>
+    <textarea id="message" name="message" required></textarea>
+
+    <input type="submit" value="Envoyer">
+</form>
+
+
